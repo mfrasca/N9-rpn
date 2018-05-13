e1d7b288097a45dfd1acb408aa44b3af00e345a6
mfrasca <mario@anche.no>
2018-05-13 12:11:09 -0500

    changing structure so script can be simpler.

--- makedeb.sh
+++ makedeb.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
+PACKAGE_NAME=rpncalc
+
 # make sure we know in which directory we are
 cd $(dirname $0)
 
 # grab version from desktop file
-VERSION=$(sed -ne 's/Version=[^0-9]*\([0-9\.]*\)/\1/p' n9-app.desktop)
+VERSION=$(sed -ne 's/Version=[^0-9]*\([0-9\.]*\)/\1/p' $PACKAGE_NAME.desktop)
 
 # make sure target dir is clean
-rm -fr rpncalc-$VERSION 2>/dev/null
+rm -fr $PACKAGE_NAME-$VERSION 2>/dev/null
 
 # copy template to target dir
-cp -a rpncalc-template rpncalc-$VERSION
+cp -a $PACKAGE_NAME-template $PACKAGE_NAME-$VERSION
 # remove placeholders, needed for storing directory structure in git, 
-find rpncalc-$VERSION -name ".placeholder" -exec rm {} \;
+find $PACKAGE_NAME-$VERSION -name ".placeholder" -exec rm {} \;
 # set version in control file
-sed -i 's/VERSION/'$VERSION'/g' rpncalc-$VERSION/DEBIAN/control
+sed -i 's/VERSION/'$VERSION'/g' $PACKAGE_NAME-$VERSION/DEBIAN/control
 
 # copy current files to target dir
-cp -p n9-app.desktop rpncalc-$VERSION/usr/share/applications/
-cp -a [a-z]* rpncalc-$VERSION/opt/rpncalc/
+cp -p $PACKAGE_NAME.desktop $PACKAGE_NAME-$VERSION/usr/share/applications/
+cp -a src/* $PACKAGE_NAME-$VERSION/opt/$PACKAGE_NAME/
 
 # don't distribute emacs backup files
-find rpncalc-$VERSION -name "*~" -exec rm {} \;
+find $PACKAGE_NAME-$VERSION -name "*~" -exec rm {} \;
 
 # HERE we make the deb file - files inside deb must be owned by root
-fakeroot bash -c 'chown -R root.root rpncalc-'$VERSION'/[a-z]*; dpkg -b rpncalc-'$VERSION
+fakeroot bash -c 'chown -R root.root '$PACKAGE_NAME'-'$VERSION'; dpkg -b '$PACKAGE_NAME'-'$VERSION
 
 # target dir is really a temporary dir, so remove it now
-rm -fr rpncalc-$VERSION 2>/dev/null
+rm -fr $PACKAGE_NAME-$VERSION 2>/dev/null
